ec9ccb5edf4435f1fbf8df7a74d0d502f3d7ce60
Nikita Khomitsevych <hamsternik9@gmail.com>
2024-03-14 03:55:03 +0000

    feat(src): migrate to astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Ni Khomitsevych';
+export const SITE_DESCRIPTION = 'Conscious Calmness, Elegant Minimalism. Software Engineer (iOS, Web) | Technical Career Adviser. At least one new post every year.';
