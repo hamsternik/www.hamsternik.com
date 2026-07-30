93aeddc1da93b38b240dceec6d5ffbfec04ca5af
Nikita Khomitsevych <hamsternik9@gmail.com>
2026-07-30 02:25:18 +0100

    edit SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ni Khomitsevych';
-export const SITE_DESCRIPTION = 'Conscious Calmness, Elegant Minimalism. Software Engineer (iOS, Web) | Technical Career Adviser. At least one new post every year.';
+export const SITE_DESCRIPTION = 'Conscious Calmness, Elegant Minimalism. Software engineer building for the web, after a decade on Apple platforms. Technical career adviser.';
